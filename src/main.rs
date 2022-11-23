15779fe7518d0941495d715c45f9c479c6b322a4
hikaru-ida <hikaru.ida@legalforce.co.jp>
2022-11-23 19:07:31 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::net::TcpListener;
 
 use rust_practice_ci::{
     configuration::get_configuration,
+    email_client::EmailClient,
     startup::run,
     telemetry::{get_subscriber, init_subscriber},
 };
@@ -17,11 +18,22 @@ async fn main() -> std::io::Result<()> {
     let connection_pool =
         PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
             .expect("Failed to create Postgres connection pool.");
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email adddress.");
+    let timeout = configuration.email_client.timeout();
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+        timeout,
+    );
 
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
     );
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool, email_client)?.await
 }
